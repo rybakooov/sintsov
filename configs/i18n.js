c8280616f594bfce1fd9f2fbd47a32eeb8d86ffd
Alexandr Rybakov <38357811+rybakooov@users.noreply.github.com>
2021-03-04 23:41:33 +0300

    create IS app

---
+++ configs/i18n.js
@@ -0,0 +1,30 @@
+// https://i18n.nuxtjs.org/setup
+export default {
+  strategy: 'prefix_except_default',
+  locales: [
+    {
+      code: 'ru',
+      name: 'Русский',
+      key: 'ru',
+    },
+    {
+      code: 'en',
+      name: 'English',
+      key: 'en-us'
+    }
+  ],
+  defaultLocale: 'ru',
+  detectBrowserLanguage: {
+    useCookie: true,
+    cookieKey: 'lang',
+    onlyOnRoot: true // recommended
+  },
+  vueI18n: {
+    fallbackLocale: 'en',
+    silentTranslationWarn: true,
+    messages:{
+      en: require('../assets/translates/en').default,
+      ru: require('../assets/translates/ru').default
+    }
+  }
+}
